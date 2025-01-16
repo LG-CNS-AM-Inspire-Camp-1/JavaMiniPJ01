4efea7893d071b928b72326801099c6c225d4d19
구동혁 <dhku@naver.com>
2025-01-16 19:57:44 +0900

    Main 수정

--- src/main/java/org/example/kudong/KUDONGMain.java
+++ src/main/java/org/example/kudong/KUDONGMain.java
@@ -9,7 +9,9 @@ import org.example.kudong.commands.ServerManageModule;
 
 /***
  * 
- * 진입점 입니다.
+ * 게임서버 플레이어 관리 (구동혁)
+ * 
+ * KUDONGMain 진입점 입니다.
  * 
  * Scanner scanner = new Scanner(System.in);
  * CLIModule module = new ServerManageModule();
@@ -20,14 +22,17 @@ import org.example.kudong.commands.ServerManageModule;
  */
 public class KUDONGMain
 {
-	public Map<String,CLIModule> map = new HashMap<>();
+	private Map<String,CLIModule> map = new HashMap<>();
 	
 	public void test(Scanner scanner)
 	{
 		while(true)
 		{
+			System.out.println("----------------------");
 			System.out.println("실행할 프로그램을 선택하세요.");
-			System.out.println("1. 구동혁 - 서버관리");
+			System.out.println("1. 구동혁 - 게임서버 플레이어 관리");
+			System.out.println("2. 홍길동 - 도서 관리");
+			System.out.println("3. 홍길동 - 영화 관리");
 			System.out.println("----------------------");
 			System.out.print("> ");
 			
@@ -35,14 +40,13 @@ public class KUDONGMain
 	        scanner.nextLine();
 
 	        if(choice.equals("-1")) {
-	        	System.out.println("프로그램을 종료합니다.");
+	        	System.out.println("▣ 프로그램을 종료합니다.");
 	        	break;
 	        }
 	        
 	        CLIModule module = map.get(choice);
 	        if(module != null) module.update(scanner);
-	        else
-	        	System.out.println("잘못된 입력값 - 다시 입력하세요.");
+	        else System.out.println("▣ 잘못된 입력값입니다.");
 		}
 	}
 	
@@ -59,11 +63,14 @@ public class KUDONGMain
 	
 	public static void main(String[] args)
 	{
+		Scanner scanner = new Scanner(System.in);
+		KUDONGMain.run(scanner);
+
+//		모듈로 실행할시 사용할것		
 //		Scanner scanner = new Scanner(System.in);
 //		KUDONGMain core = new KUDONGMain();
 //		core.registerModule("1", new ServerManageModule());
 //		core.test(scanner);
-		Scanner scanner = new Scanner(System.in);
-		KUDONGMain.run(scanner);
 	}
+
 }
